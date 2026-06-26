4d8c21067f2fec7d6d238646e397bfbbe8eb36ba
Maria <tu mail>
2026-06-26 10:54:00 -0300

    Agregar ejercicio ListaAmigos

---
+++ ListaAmigos.py
@@ -0,0 +1,21 @@
+# Lista (Nombre amigos)
+nombres = ["Ana", "Luis", "Carlos", "Maria"]
+
+print(nombres)
+
+# Muestro el primer nombre
+print(nombres[0])
+print ("\n ---------------")
+
+# Muestro el último amigo
+print(nombres[-1])
+print ("\n ---------------")
+
+# Cambiar el segundo nombre
+nombres[1] = "María"
+print ("\n ---------------")
+
+# Muestro la lista completa
+print(nombres)
+print ("\n ---------------")
+
