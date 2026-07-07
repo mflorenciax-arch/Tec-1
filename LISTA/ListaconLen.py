35ff22138898d794a6320b59de9bc071c972993f
Maria <tumail@gmail.com>
2026-07-07 10:39:16 -0300

    ListaconLen.py

---
+++ LISTA/ListaconLen.py
@@ -0,0 +1,9 @@
+# 1. Crear una lista con mis materias favoritas
+materias = ["Programación", "Matemática", "Inglés", "Base de Datos"]
+
+# 2. Mostrar cuántas materias tengo
+print("Cantidad de materias:", len(materias))
+
+# 3. Mostrar la primera y la última usando len()
+print("Primera materia:", materias[0])
+print("Última materia:", materias[len(materias) - 1])
