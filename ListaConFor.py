574af06d3b52c699e7973a2cda4c3ef1f1ed6970
Maria <tu mail>
2026-07-07 10:20:33 -0300

    Agregar ejercicio ListaConFor

---
+++ ListaConFor.py
@@ -0,0 +1,8 @@
+#Lista de numeron con For
+# 1. Crear la lista
+numeros = [2, 4, 6, 8, 10]
+
+# 2. Recorrer la lista y mostrar cada número multiplicado por 2
+for numero in numeros:
+    print(numero * 2)
+    print(numero * 2)
